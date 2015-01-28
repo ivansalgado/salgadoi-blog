37f65728463900ea1ad97036e72af090d68a610d
Ivan Salgado <ivansalgado2000@gmail.com>
2015-01-27 19:13:57 -0800

    completed first basic milestone

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Web server is running locally";
+        echo "web server is running locally";
         ?>
     </body>
 </html>
