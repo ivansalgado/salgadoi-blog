8ca80a5d64029d42378213e5e16e891fae2dcd9e
Ivan Salgado <ivansalgado2000@gmail.com>
2015-02-11 20:51:07 -0800

    done with video 35 fixed error

--- model/Database.php
+++ model/Database.php
@@ -1,66 +1,65 @@
 <?php
 
-class Database{
+class Database {
+
     private $connection;
     private $host;
     private $username;
     private $password;
     private $database;
     public $error;
-    
+
     public function __construct($host, $username, $password, $database) {
-        $this->host;
-        $this->username;
-        $this->password;
-        $this->database;
-        
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+
         $this->connection = new mysqli($host, $username, $password);
         
-    if ($this->connection->connect_error){
-        die("<p>Error: " . $this->connection->connect_error . "</p>");
-    }
-    
-    
-    $exists = $this->connection->select_db($database);
-    
-    if(!$exists){
-        $query = $this->connection->query("CREATE DATABASE $database");
+        if ($this->connection->connect_error) {
+            die("<p>Error: " . $this->connection->connect_error . "</p>");
+        }
+
+        $exists = $this->connection->select_db($database);
         
-        if($query){
-            echo "<p>Successfully created database" . $database . "</p>";
+        if (!$exists) {
+            $query = $this->connection->query("CREATE DATABASE $database");
+
+            if ($query) {
+                echo "<p>Successfully created database" . $database . "</p>";
+            }
+        } else {
+            echo "<p>Database already exists</p>";
         }
     }
-    else{
-        echo "<p>Database already exists</p>";
-    }
-        
-    }
-    
+
     public function openConnection() {
-       $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
-       
-       if ($this->connection->connect_error) {
-           die ("<p>Error: " . $this->connection->connect_error . "</p>");
-       }
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        if ($this->connection->connect_error) {
+            die("<p>Error: " . $this->connection->connect_error . "</p>");
+        }
     }
-    
+
     public function closeConnection() {
-       if (isset($this->connection)){
-           $this->connection->close();
-       }
+        if (isset($this->connection)) {
+            $this->connection->close();
+        }
     }
-    
+
     public function query($string) {
-       $this->openConnection();
-       
-       $query = $this->connection->query($string);
-       
-       if(!$query){
-           $this->error = $this->connection->error;
-       }
-       
-       $this->closeConnection();
-       
-       return $query;
+        $this->openConnection();
+
+        $query = $this->connection->query($string);
+
+        if (!$query) {
+            $this->error = $this->connection->error;
+        }
+
+        $this->closeConnection();
+
+        return $query;
     }
+
 }
--- view/navigation.php
+++ view/navigation.php
@@ -5,7 +5,7 @@
 <nav>
     <ul>
         <li>
-            <a href="<?php echo $path . "post.php" ?>"><button type="button" class="btn">Blog Post Form</button></a>
+            <a href="<?php echo $path . "post.php" ?>"><button type="button" class="btn"><h1>Blog Post Form</h1></button></a>
         </li>
     </ul>
 </nav>
