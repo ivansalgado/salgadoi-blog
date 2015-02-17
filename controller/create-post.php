f21fe35c95ffe8942819469c708a6be250b0fc9e
Ivan Salgado <ivansalgado2000@gmail.com>
2015-02-17 09:18:20 -0800

    fixed

--- controller/create-post.php
+++ controller/create-post.php
@@ -4,7 +4,7 @@
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
     
-    $query = $connection->query("INSERT INTO post SET title = '$title', post = '$post'");
+    $query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
     
     if ($query) {
         echo "<p>Successfully inserted post: $title</p>";
