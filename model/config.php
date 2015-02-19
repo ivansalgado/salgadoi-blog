b19e2ee3587cb838550880ded809699df59f710d
Ivan Salgado <ivansalgado2000@gmail.com>
2015-02-19 09:18:49 -0800

    fixed my problem

--- model/config.php
+++ model/config.php
@@ -10,7 +10,7 @@
     $password  = "root";
     $database = "blog_db";
     
-    if(isset($_SESSION["connection"])){
+    if(!isset($_SESSION["connection"])){
         $connection = new Database($host, $username, $password, $database);
         $_SESSION["connection"] = $connection;
     }
