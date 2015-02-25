423e2317518d700702eea92ba6ca5d01e73dd3d6
Ivan Salgado <ivansalgado2000@gmail.com>
2015-02-25 08:32:08 -0800

    added css

--- view/header.php
+++ view/header.php
@@ -7,13 +7,44 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <link rel="shortcut icon" type="image/x-icon" href="ico/á.ico">
         <link type='text/css' rel='stylesheet' href='css/bootstrap-theme.css'>
         <link type='text/css' rel='stylesheet' href='css/bootstrap.css'>
         <link type='text/css' rel='stylesheet' href='css/css.css'>
         <link href='http://fonts.googleapis.com/css?family=Carme' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Raleway:600' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
         <title>Iván Salgado Blog</title>
     </head>
     <body>  
+        <nav class="navbar navbar-default" role="navigation" id="nav">
+  <div class="container-fluid">
+    
+    <div class="navbar-header">
+        <div class="navbar-brand" href="index.html" id='i'>Iván Salgado <a href="" id="v">Style</a></div>
+    </div>
+
+    
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="">Blog<span class="sr-only">(current)</span></a></li>
+        <li><a href="http://localhost:8383/salgadoi-portfolio2/index.html">Portfolio</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Gallery</a></li>
+        
+        <li><a href="http://www.theappacademy.us/">The App Academy</a></li>
+        <li><a href="http://phs.pusd.us/pages/Pasadena_High">Pasadena High School</a></li>
+        </ul>
+        
+        <ul class="nav navbar-nav navbar-right">
+        <li><a href="http://localhost/salgadoi-blog/view/login-form.php">Sign in</a></li>
+        <li><a href="http://localhost/salgadoi-blog/view/register-form.php">Register</a></li>
+        <li><div class="btn-group"><a href="http://php.net/">
+        <button type="button" class="btn btn-default" id="html">php</button></a>
+        </div></li>
+        
+      </ul>
+    </div>
+  </div>
+  </nav>       
